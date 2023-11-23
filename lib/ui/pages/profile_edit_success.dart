07a73c58ebeae111b49a8d922a53b1e379333c62
Ahmad Saifur Rohman <55994225+ahsar04@users.noreply.github.com>
2023-11-23 21:16:28 +0700

    profile edit success page

---
+++ lib/ui/pages/profile_edit_success.dart
@@ -0,0 +1,48 @@
+import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widgets/buttons.dart';
+import 'package:flutter/material.dart';
+
+class ProfileEditSucces extends StatelessWidget {
+  const ProfileEditSucces({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Nice Update!",
+              style: blackTextStyle.copyWith(
+                fontSize: 20,
+                fontWeight: semiBold,
+              ),
+            ),
+            const SizedBox(
+              height: 26,
+            ),
+            Text(
+              "Your data is safe with \nour system",
+              style: blackTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: reguler,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            CustomFilledButton(
+              title: "My Profile",
+              width: 190,
+              onPressed: () {
+                Navigator.pushNamed(context, "/profile");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
