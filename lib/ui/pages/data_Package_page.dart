014c5f9668308cd3a804408a9db70c8ccc91ca85
Ahmad Saifur Rohman <55994225+ahsar04@users.noreply.github.com>
2023-11-26 12:19:51 +0700

    data package page

---
+++ lib/ui/pages/data_Package_page.dart
@@ -0,0 +1,40 @@
+import 'package:bank_sha/shared/theme.dart';
+import 'package:bank_sha/ui/widgets/form.dart';
+import 'package:flutter/material.dart';
+
+class DataPackagePage extends StatelessWidget {
+  const DataPackagePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Paket Data"),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          Text(
+            "Phone Number",
+            style: blackTextStyle.copyWith(
+              fontWeight: semiBold,
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          const CustomFormField(
+            title: "+628",
+            isShowTitle: false,
+          )
+        ],
+      ),
+    );
+  }
+}
